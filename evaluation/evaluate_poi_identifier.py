b01f246a1b486f9c5b4b3e7f920626a2722a13ca
Akim Akimov <zedmor@gmail.com>
2016-05-28 20:07:02 -0400

    zzzz

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -12,6 +12,9 @@
 """
 
 import pickle
+from sklearn import tree
+from sklearn import metrics
+from sklearn.cross_validation import train_test_split
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
@@ -29,3 +32,27 @@ labels, features = targetFeatureSplit(data)
 ### your code goes here 
 
 
+### it's all yours from here forward!
+features_train, features_test, labels_train, labels_test = train_test_split(features,labels,test_size=0.3,random_state=42)
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+#clf.fit(features,labels)
+predict = clf.predict(features_test)
+print clf.score(features_test,labels_test)
+print metrics.precision_score(labels_test,predict)
+print metrics.recall_score(labels_test,predict)
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
+print metrics.precision_score(true_labels,predictions)
+
+print metrics.recall_score(true_labels,predictions)
+
+#print metrics.
+#print labels_test.count(1.0)
+#print len(labels_test)
+#print clf.predict(features_test)
+#for i in range (0,len(labels_test)):
+#    if labels_test[i]==clf.predict(features_test[i])==1.0:
+#        print i
--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -11,6 +11,8 @@
 """
 
 import pickle
+from sklearn import tree
+from sklearn.cross_validation import train_test_split
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
@@ -26,7 +28,13 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+true labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
 
-### it's all yours from here forward!  
 
 
+### it's all yours from here forward!
+features_train, features_test, labels_train, labels_test = train_test_split(features,labels,test_size=0.3,random_state=42)
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+print clf.score(features_test,labels_test)
